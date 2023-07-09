5797dacd55bde0edfe8fda502d2913bab2a052e3
dnogues89 <damiannogues@gmail.com>
2023-07-09 15:44:19 -0300

    contact.html appart, and inclueded at the index

--- WhatApp/views.py
+++ WhatApp/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render , HttpResponse
-
+from django.shortcuts import render , redirect
+from contacto.forms import ContactForm
 
 # Create your views here.
 
 def home(requests):
-    return render(requests,'WhatApp/index.html')
+    #This includes contact form at the index
+    contact_form = ContactForm()
+    if requests.method == 'POST':
+        contact_form=ContactForm(data=requests.POST)
+        if contact_form.is_valid():
+            nombre=requests.POST.get('name')
+            email=requests.POST.get('email')
+            message=requests.POST.get('message')
+            return redirect("/?valid")
+    context = {"my_form":contact_form}
+    return render(requests,'WhatApp/index.html',context)
 
 def prueba (requests):
     return render(requests,'WhatApp/prueba.html')
--- contacto/views.py
+++ contacto/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import ContactForm
 
 # Create your views here.
 def landing(requests):
     contact_form = ContactForm()
-    
+    if requests.method == 'POST':
+        contact_form=ContactForm(data=requests.POST)
+        if contact_form.is_valid():
+            nombre=requests.POST.get('name')
+            email=requests.POST.get('email')
+            message=requests.POST.get('message')
+            return redirect("/contacto/?valid")
     context = {"my_form":contact_form}
     return render(requests,'contacto/landing.html',context)
