dbb43ff88af3fb899b916c9e0d9698b178ec6b7e
dnogues89 <damiannogues@gmail.com>
2023-07-10 13:19:41 -0300

    .

--- web_bot/settings.py
+++ web_bot/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'web_bot.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [''],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
